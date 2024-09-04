235afa3304650896c0261114694bb26d7ff7632e
Андрей Андронов <andrew270504cska@gmail.com>
2024-09-04 17:43:09 +0300

    done

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,8 @@ export default{
     <!-- Контент -->
     <div class="max-w-7xl w-full max-h-4/5 p-4 mx-2 rounded-xl backdrop-blur-xl border-2 border-white/50 bg-white/15 flex flex-col items-center justify-center gap-8">
       <defaultInput v-model="searchInput" @input="searchTask" v-if="tasks.length > 0" type="text" :placeholder="'Поиск'" />
-      <div v-if="searchResults && searchInput.length > 0 && tasks.length > 0" class="border-2 border-white/50 py-2 flex flex-col items-center gap-4 overflow-y-auto w-full h-full">
+      <!-- Результаты поиска (уже не стал переиспользовать компонент) -->
+        <div v-if="searchResults && searchInput.length > 0 && tasks.length > 0" class="border-2 border-white/50 py-2 flex flex-col items-center gap-4 overflow-y-auto w-full h-full">
         <p v-if="searchResults.length > 0">Результаты поиска:</p>
         <div v-for="task in searchResults" :key="task.id" class="flex flex-row items-center w-full justify-between px-4 py-2 border-2 border-white text-white" :class="task.isCompleted ? 'text-opacity-50 bg-white/5 border-opacity-25' : ''">
           <p class="text-xl font-semibold">{{task.name}}</p>
@@ -60,6 +61,7 @@ export default{
         </div>
         <p v-if="searchResults.length < 1">Нет результатов</p>
       </div>
+      <!-- Все таски -->
       <div v-if="tasks" class="flex flex-col items-center gap-4 overflow-y-auto w-full h-full">
         <div v-for="task in tasks" :key="task.id" class="flex flex-row items-center w-full justify-between px-4 py-2 border-2 border-white text-white" :class="task.isCompleted ? 'text-opacity-50 bg-white/5 border-opacity-25' : ''">
           <p class="text-xl font-semibold">{{task.name}}</p>
@@ -69,6 +71,7 @@ export default{
           </div>
         </div>
       </div>
+      <!-- Добавление задачи -->
       <div class="flex flex-row items-center w-full">
         <defaultInput @keydown-enter="addTask" v-model="taskInput" :placeholder="'Введите задание:'" />
         <defaultButton @click="addTask" class="text-white">Добавить</defaultButton>
