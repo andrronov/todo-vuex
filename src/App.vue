166f2c8e992217567dbb217327ec01881380e51b
Андрей Андронов <andrew270504cska@gmail.com>
2024-09-06 15:06:19 +0300

    done

--- src/App.vue
+++ src/App.vue
@@ -42,32 +42,37 @@ export default{
 
 <template>
   <div class="w-full h-screen flex items-center justify-center bg-indigo-800 text-xl relative overflow-hidden">
-
     <!-- Фоновые мячики -->
     <div class="w-56 h-56 bottom-5 right-5 bg-green-700/90 blur-sm rounded-full absolute" id="ball_1"></div>
     <div class="w-44 h-44 top-5 left-5 bg-red-700/90 blur-sm rounded-full absolute" id="ball_2"></div>
     
     <!-- Контент -->
-    <div class="max-w-7xl relative w-full max-h-4/5 p-4 mx-2 rounded-xl backdrop-blur-xl border-2 border-white/50 bg-white/15 flex flex-col items-center justify-center gap-8">
+    <div class="max-w-7xl relative w-full max-h-[80vh] p-4 mx-2 rounded-xl backdrop-blur-xl border-2 border-white/50 bg-white/15 flex flex-col items-center justify-center gap-8">
       <defaultInput v-model="searchInput" @input="searchTask" v-if="tasks.length > 0" type="text" :placeholder="'Поиск'" />
-      <!-- Результаты поиска  -->
-        <div v-if="searchResults && searchInput.length > 0 && tasks.length > 0" class="border-2 border-white/50 py-2 flex flex-col items-center gap-4 overflow-y-auto w-full h-full">
+
+      <!-- Результаты поиска -->
+      <div v-if="searchResults && searchInput.length > 0 && tasks.length > 0" class="border-2 border-white/50 py-2 flex flex-col items-center gap-4 overflow-y-auto w-full max-h-[50vh]">
         <p v-if="searchResults.length > 0">Результаты поиска:</p>
         <taskComponent v-for="task in searchResults" :key="task.id" :task="task" @update="updateTask" @delete="deleteTask" />
         <p v-if="searchResults.length < 1">Нет результатов</p>
       </div>
+
       <!-- Все таски -->
-      <taskComponent v-for="task in tasks" :key="task.id" :task="task" @update="updateTask" @delete="deleteTask" />
+      <div class="flex flex-col overflow-y-auto gap-4 w-full max-h-[50vh]">
+        <taskComponent v-for="task in tasks" :key="task.id" :task="task" @update="updateTask" @delete="deleteTask" />
+      </div>
+
       <!-- Добавление задачи -->
       <div class="flex flex-row items-center w-full">
         <defaultInput @keydown-enter="addTask" v-model="taskInput" :placeholder="'Введите задание:'" />
         <defaultButton @click="addTask" class="text-white">Добавить</defaultButton>
       </div>
     </div>
- </div>
+</div>
+
 </template>
 
-<style scoped>
+<style>
 @keyframes move1 {
   0% {
     transform: translate(0, 0);
--- src/components/taskComponent.vue
+++ src/components/taskComponent.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="flex flex-col items-center gap-4 overflow-y-auto w-full h-full">
+   <div class="flex flex-col items-center gap-4 w-full">
       <div :key="task.id" class="flex flex-row items-center w-full justify-between px-4 py-2 border-2 border-white text-white" :class="task.isCompleted ? 'text-opacity-50 bg-white/5 border-opacity-25' : ''">
         <p class="text-xl font-semibold">{{task.name}}</p>
         <div class="flex flex-row items-center gap-4">
