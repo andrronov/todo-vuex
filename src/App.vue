af1b94386842ee116548a107dc60b3b881d8d0a6
Андрей Андронов <andrew270504cska@gmail.com>
2024-09-06 14:35:08 +0300

    changed balls

--- src/App.vue
+++ src/App.vue
@@ -44,11 +44,11 @@ export default{
   <div class="w-full h-screen flex items-center justify-center bg-indigo-800 text-xl relative overflow-hidden">
 
     <!-- Фоновые мячики -->
-    <div class="w-56 h-56 bottom-5 right-5 bg-green-700 rounded-full absolute" id="ball_1"></div>
-    <div class="w-44 h-44 top-5 left-5 bg-red-700 rounded-full absolute" id="ball_2"></div>
+    <div class="w-56 h-56 bottom-5 right-5 bg-green-700/90 blur-sm rounded-full absolute" id="ball_1"></div>
+    <div class="w-44 h-44 top-5 left-5 bg-red-700/90 blur-sm rounded-full absolute" id="ball_2"></div>
     
     <!-- Контент -->
-    <div class="max-w-7xl w-full max-h-4/5 p-4 mx-2 rounded-xl backdrop-blur-xl border-2 border-white/50 bg-white/15 flex flex-col items-center justify-center gap-8">
+    <div class="max-w-7xl relative w-full max-h-4/5 p-4 mx-2 rounded-xl backdrop-blur-xl border-2 border-white/50 bg-white/15 flex flex-col items-center justify-center gap-8">
       <defaultInput v-model="searchInput" @input="searchTask" v-if="tasks.length > 0" type="text" :placeholder="'Поиск'" />
       <!-- Результаты поиска  -->
         <div v-if="searchResults && searchInput.length > 0 && tasks.length > 0" class="border-2 border-white/50 py-2 flex flex-col items-center gap-4 overflow-y-auto w-full h-full">
